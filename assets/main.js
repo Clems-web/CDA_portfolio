82df9c9c24982fb1e85d083fd3c18ae6d926babd
Clems-web <clement.servais1@gmail.com>
2021-12-10 11:40:07 +0100

    added language's part

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,11 @@ cubeContainer.append(new cubeCrea('#4e9f1e', '#38ff03'));
 let cubeContainer2 = document.getElementById('secondCubeContainer');
 cubeContainer2.append(new cubeCrea('#FF00FFBF', '#FF00FFFF'));
 
+let cubeContainer3 = document.getElementById('thirdCubeContainer');
+cubeContainer3.append(new cubeCrea('#28dee6', '#00FFFFFF'));
+
+
+
 
 
 const presentation = document.querySelector(".js-tilt");
